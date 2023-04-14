151f33554945ea2408b07e7af57cc68e80e6e7a4
Jeremia Kimelman <jbkimelman@gmail.com>
2023-04-14 12:31:36 -0700

    Initial up

---
+++ README.md
@@ -0,0 +1,7 @@
+# California financial disclosure data
+
+## Data use
+
+If you use this dataset, please mention it was collected and cleaned by CalMatters. If you have any questions about this dataset, feel free to contact us.
+
+[CalMatters](https://calmatters.org/) is a nonpartisan, nonprofit journalism venture committed to explaining how California’s state Capitol works and why it matters.
---
+++ scripts/index.ts
@@ -0,0 +1,39 @@
+import { csvFormat } from 'npm:d3-dsv'
+
+import scheduleA from './schedule-a.ts'
+import scheduleD from './schedule-d.ts'
+import scheduleE from './schedule-e.ts'
+
+console.log(`Fetching data from API`)
+const apiUrl = `https://calmatters-disclosure-disco.netlify.app/forms.json`
+const response = await fetch(apiUrl)
+const forms = await response.json()
+
+const outputs = [
+  {
+    filepath: 'schedule-a.csv',
+    transformer: scheduleA
+  },
+  {
+    filepath: 'schedule-d.csv',
+    transformer: scheduleD
+  },
+  {
+    filepath: 'schedule-e.csv',
+    transformer: scheduleE
+  }
+]
+
+console.log(`Found ${forms.length} forms, turning into ${outputs.length} CSV files`)
+
+const out = outputs.map(async output => {
+  const { filepath, transformer } = output
+  const data = transformer(forms)
+  const csv = csvFormat(data)
+
+  console.log(`Saving ${data.length.toLocaleString('en-US')} rows to ${filepath}`)
+  await Deno.writeTextFile(filepath, csv)
+})
+await Promise.all(out)
+
+console.log(`Done`)
